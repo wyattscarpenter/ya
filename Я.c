fb9c47bdf09f16e355c0c2989bdc29482564fc8a
wyattscarpenter <wyattscarpenter@gmail.com>
2018-09-11 22:11:50 -0500

    Update Я.c

--- Я.c
+++ Я.c
@@ -1,4 +1,4 @@
 //the c in this file name is short for communism
 int main(int argc, char** argv){
-	printf("The answer is: whatever our glorious Comrade Stalin has predicted.")
+	printf("The answer is: whatever our glorious Comrade Stalin has predicted.\n");
 }
